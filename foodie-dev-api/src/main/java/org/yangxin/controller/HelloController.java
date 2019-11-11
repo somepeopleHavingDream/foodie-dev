83b72eb5a53fac751fc45c52d08251ce2f64de16
yangxin <1393003255@qq.com>
2019-11-11 21:52:54 +0800

    feat: 聚合工程整合SpringBoot

---
+++ foodie-dev-api/src/main/java/org/yangxin/controller/HelloController.java
@@ -0,0 +1,12 @@
+package org.yangxin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public Object hello() {
+        return "Hello World!";
+    }
+}
