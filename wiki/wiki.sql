f2fcf29f2cad5ac35de92705d3638c721adac7d5
yangxin <1393003255@qq.com>
2019-11-28 10:50:07 +0800

    feat: 商品评价 - 编写自定义mapper查询

--- wiki/wiki.sql
+++ wiki/wiki.sql
@@ -1,20 +1,11 @@
-select
-       c.id as rootCatId,
-       c.name as rootCatName,
-       c.slogan as slogan,
-       c.cat_image as catImage,
-       c.bg_color as bgColor,
-       i.id as itemId,
-       i.item_name as itemName,
-       ii.url as itemUrl,
-       i.created_time as createdTime
-from category c
-left join items i
-on c.id = i.root_cat_id
-left join items_img ii
-on i.id = ii.item_id
-where c.type = 1
-and i.root_cat_id = 7
-and ii.is_main = 1
-order by i.created_time desc
-limit 0, 6;
+select ic.comment_level as commentLevel,
+    ic.content as content,
+    ic.sepc_name as specName,
+    ic.created_time as createdTime,
+    u.face as userFace,
+    u.nickname as nickname
+from items_comments ic
+left join users u
+on ic.user_id = u.id
+where ic.item_id = 'cake-1001'
+and ic.comment_level = 1;
