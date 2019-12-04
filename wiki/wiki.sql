3c5516d3325ce184d71f8e56c6d0b7056e33f700
yangxin <1393003255@qq.com>
2019-12-04 13:53:45 +0800

    feat: 购物车 - 渲染(刷新)购物车 - 上

--- wiki/wiki.sql
+++ wiki/wiki.sql
@@ -1,20 +1,15 @@
-select i.id                    as itemId,
-       i.item_name             as itemName,
-       i.sell_counts           as sellCounts,
-       ii.url                  as imgUrl,
-       tempSpec.price_discount as price
-from items i
-         left join items_img ii
-                   on i.id = ii.item_id
-         left join
-     (
-         select item_id, min(price_discount) as price_discount
-         from items_spec
-         group by item_id
-     ) tempSpec
-     on i.id = tempSpec.item_id
-where ii.is_main = 1;
-
-select item_id, min(price_discount) as price_discount
-from items_spec
-group by item_id;
+select t_items.id as itemId,
+       t_items.item_name as itemName,
+       t_items_img.url as itemImgUrl,
+       t_items_spec.id as specId,
+       t_items_spec.name as specName,
+       t_items_spec.price_discount as priceDiscount,
+       t_items_spec.price_normal as priceNormal
+from items_spec t_items_spec
+left join items t_items
+on t_items.id = t_items_spec.item_id
+left join items_img t_items_img
+on t_items_img.item_id = t_items.id
+where t_items_img.is_main = 1
+and t_items_spec.id
+in ('1', '3', '5')
