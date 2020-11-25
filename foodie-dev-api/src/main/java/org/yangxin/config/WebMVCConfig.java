c3d51dff4e3ca87624dca5fac114b51a270afa62
yangxin <1393003255@qq.com>
2020-11-25 21:22:59 +0800

    feat: 上传头像-为静态资源提供网络映射服务

--- foodie-dev-api/src/main/java/org/yangxin/config/WebMVCConfig.java
+++ foodie-dev-api/src/main/java/org/yangxin/config/WebMVCConfig.java
@@ -4,6 +4,8 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * mvc
@@ -12,10 +14,22 @@ import org.springframework.web.client.RestTemplate;
  * 2020/03/19 21:02
  */
 @Configuration
-public class WebMVCConfig {
+public class WebMVCConfig implements WebMvcConfigurer {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
+    }
+
+    /**
+     * 实现静态资源的映射
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                // 映射swagger2
+                .addResourceLocations("classpath:/META-INF/resources/")
+                // 映射本地静态资源
+                .addResourceLocations("file:/home/yangxin/Projects/foodie-store/");
     }
 }
